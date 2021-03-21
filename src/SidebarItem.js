7f84bb9586709930280f5b7a7eff61071f45cf75
petvi <petrasvilkelis@gmail.com>
2021-03-21 13:41:16 +0100

    Finish design

--- src/SidebarItem.js
+++ src/SidebarItem.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function SidebarItem() {
+export default function SidebarItem({
+  name,
+  active,
+  handleClick,
+}) {
   return (
-    <button className="sidebar-item">
-      Sidebar item
+    <button
+      className={`sidebar-item ${
+        active ? 'active' : ''
+      }`}
+      onClick={handleClick}
+    >
+      {name}
     </button>
   )
 }
--- src/Slider.js
+++ src/Slider.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function Slider() {
+export default function Slider({
+  min,
+  max,
+  value,
+  handleChange,
+}) {
   return (
     <div className="slider-container">
-      <input type="range" className="slider" />
+      <input
+        type="range"
+        className="slider"
+        min={min}
+        max={max}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   )
 }
